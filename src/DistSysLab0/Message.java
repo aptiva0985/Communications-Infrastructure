f9fd38e7282888a65795649913ecdefe97f8aa43
Yuanshuo Cui <aptiva0985@gmail.com>
2014-01-18 10:11:12 -0500

    Basic interface

---
+++ src/DistSysLab0/Message.java
@@ -0,0 +1,64 @@
+package DistSysLab0;
+
+import java.io.Serializable;
+
+import org.apache.log4j.Logger;
+
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+    static Logger logger = Logger.getLogger(Message.class);
+    
+    private String src;
+    private String dest;
+    private String kind;
+    private Object data;
+
+    /**
+     * Constructor of message
+     * @param dest
+     * @param kind
+     * @param data
+     */
+    public Message(String dest, String kind, Object data) {
+        this.dest = dest;
+        this.kind = kind;
+        this.data = data;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public String getDest() {
+        return dest;
+    }
+
+    public void setDest(String dest) {
+        this.dest = dest;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+    
+    @Override
+    public String toString() {
+        return this.src + " to " + this.dest + ". Kind: "  + this.kind + " with " + this.data;
+    }   
+}
--- src/DistSysLab0/MessagePasser.java
+++ src/DistSysLab0/MessagePasser.java
@@ -1,1 +1,80 @@
 package DistSysLab0;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.apache.log4j.Logger;
+
+public class MessagePasser {
+    private static MessagePasser instance;
+    private static Logger logger = Logger.getLogger(MessagePasser.class);
+    
+    private Queue<Message> sendQueue = new LinkedList<Message>();
+    private Queue<Message> recvQueue = new LinkedList<Message>();
+    
+    private String configFile;
+    private String localName;
+    private int port;
+    
+    /**
+     * Actual constructor for MessagePasser
+     * @param configFile
+     * @param localName
+     */
+    private MessagePasser(String configFile, String localName) {
+        this.localName = localName;
+        this.configFile = configFile;
+        this.port = 0;
+        this.init();    
+        logger.debug(this.toString());
+    }
+    
+    /**
+     * Singleton constructor for MessagePasser
+     * @param configuration_filename
+     * @param local_name
+     */
+    public static synchronized MessagePasser getInstance(String configuration_filename, String local_name) {
+        if (instance == null) {
+            instance = new MessagePasser(configuration_filename, local_name);
+        }
+        return instance;
+    }
+    
+    /**
+     * Return existed instance of MessagePasser
+     * @return instance
+     */
+    public static MessagePasser getInstance() {
+        return instance;
+    }
+
+    /**
+     * Peek a message from receive queue.
+     * @return A message.
+     */
+    public synchronized Message receive() {
+        Message msg = recvQueue.peek();
+        if(msg != null) {
+            recvQueue.remove();
+            return msg;
+        }
+        return null;
+    }
+    
+    public synchronized void init() {
+        
+    }
+
+    /**
+     * For test.
+     */
+    public static void main(String[] args) {
+
+    }
+    
+    @Override
+    public String toString() {
+        return "";
+    }
+}
