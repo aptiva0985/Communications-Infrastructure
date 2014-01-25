046b1bb343d2dc5fef4df89ab325805528141ec8
Jacky Chen <jingchen@cmu.edu>
2014-01-24 19:02:47 -0800

    fix a merge mistake

--- src/DistSysLab0/Simulator.java
+++ src/DistSysLab0/Simulator.java
@@ -4,27 +4,30 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class Simulator {
-    static public UserInputThread uiThread;
-    private static String[] arg;
-
-    public static void startUIThread() throws IOException {
-        uiThread = new UserInputThread();
-        Thread thread = new Thread(uiThread);
+	static public UserInputThread uiThread;
+	private static String[] arg;
+	
+	public static void startUIThread() {
+		uiThread = new UserInputThread();
+        Thread thread = new Thread(uiThread); 
         thread.start();
     }
-    /**
-     * Test entrance.
-     */
-    public static void main(String[] args) throws IOException {
-        try {
-            arg = UserInputThread.init();
-            MessagePasser messagePasser = MessagePasser.getInstance(arg[0], arg[1]);
-            messagePasser.startListener();
-            messagePasser.startSender();
-            startUIThread();
+	/**
+	 * For test.
+	 */
+	public static void main(String[] args) throws IOException {
+		try {
+			arg = UserInputThread.init();
+            MessagePasser messagePasser = MessagePasser.getInstance(arg[0], arg[1]);	
+			messagePasser.startListener();
+			messagePasser.startSender();		
+			startUIThread();
+	        //messagePasser.teminate();			
+				
+		} catch (UnknownHostException e) { 
+			// Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-    }
 }
